04da4a78d61a8ee6a87b4d52d559c91e86650886
Mayra016 <111470957+Mayra016@users.noreply.github.com>
2023-06-20 09:27:22 -0300

    Update Sorteo.py
    Implementación de la función quitarRepetidos()

--- Sorteo.py
+++ Sorteo.py
@@ -1,8 +1,28 @@
 import random
-números = []
-i = 1
-while i <= 6:
-    num = random.randrange(1, 60)
-    números.append(num)
-    i = i + 1
-print(números)    
+numeros = []
+
+def generar():
+    global numeros
+    i = 1
+    while i <= 6:
+        num = random.randrange(1, 60)
+        numeros.append(num)
+        i = i + 1
+    return quitarRepetidos()
+    
+def quitarRepetidos():
+    global numeros
+    numeros = set(numeros)
+    print(numeros)
+    while len(numeros) != 6:
+        repetidos = 6 - len(numeros)
+        i = 1
+        while i <= repetidos:
+            num = random.randrange(1, 60)
+            numeros.add(num)
+            i = i + 1
+            print(numeros)
+        numeros = set(numeros)
+    return numeros      
+            
+print(generar())    
