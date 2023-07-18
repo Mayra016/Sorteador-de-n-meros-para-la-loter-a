7c12d2e12eadf0fac5bfd8bb22925d2b0ad50538
Mayra016 <111470957+Mayra016@users.noreply.github.com>
2023-07-18 09:35:19 -0300

    Update Sorteo.py
    Optimización de código

--- Sorteo.py
+++ Sorteo.py
@@ -1,28 +1,11 @@
 import random
-numeros = []
-
-def generar():
-    global numeros
-    i = 1
-    while i <= 6:
-        num = random.randrange(1, 60)
-        numeros.append(num)
-        i = i + 1
-    return quitarRepetidos()
-    
-def quitarRepetidos():
-    global numeros
-    numeros = set(numeros)
-    print(numeros)
-    while len(numeros) != 6:
-        repetidos = 6 - len(numeros)
-        i = 1
-        while i <= repetidos:
-            num = random.randrange(1, 60)
-            numeros.add(num)
-            i = i + 1
-            print(numeros)
-        numeros = set(numeros)
-    return numeros      
             
-print(generar())    
+def generar2():
+    nums = []
+    while len(nums) < 6:
+        num = random.randrange(1, 60)
+        if num not in nums:
+            nums.append(num)
+    return nums        
+    
+print(generar2())    
